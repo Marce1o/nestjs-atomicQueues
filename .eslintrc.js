e4e3d9d2d01c9eaa7b9565c8b7b37670ea9b27bc
marselo <lmruizher@gmail.com>
2026-04-22 23:55:09 -0600

    chore: add ESLint + Prettier config, format codebase
    - Add .eslintrc.js with @typescript-eslint recommended + no-explicit-any: warn
    - Add .prettierrc with single quotes, trailing commas, 100 char width
    - Add format/format:check scripts to package.json
    - Add lint and format:check steps to CI workflow
    - Install prettier as dev dependency

---
+++ .eslintrc.js
@@ -0,0 +1,24 @@
+/** @type {import('eslint').Linter.Config} */
+module.exports = {
+  root: true,
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    project: './tsconfig.json',
+    sourceType: 'module',
+  },
+  plugins: ['@typescript-eslint'],
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+  ],
+  env: {
+    node: true,
+    jest: true,
+  },
+  rules: {
+    '@typescript-eslint/no-explicit-any': 'warn',
+    '@typescript-eslint/no-unused-vars': ['warn', { argsIgnorePattern: '^_' }],
+    '@typescript-eslint/no-empty-function': 'off',
+  },
+  ignorePatterns: ['dist/', 'node_modules/', 'candy-shop/'],
+};
